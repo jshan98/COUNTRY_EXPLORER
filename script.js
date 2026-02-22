734906d4a189d3cc4e96f02049c364be90cfde06
jshan98 <jshanahan@ggc.edu>
2026-02-22 17:33:21 -0500

    created the formattedName function to format lists in each object and added country card click functionality.

--- script.js
+++ script.js
@@ -7,10 +7,31 @@ var searchInput = document.getElementById("search-input"); // Holds the name sea
 var regionInput = document.getElementById("region-select"); // Holds the region select drop down element
 var populationInput = document.getElementById("population-input"); // Holds the population search bar element
 var showMorePressed = document.getElementById("show-more-btn"); // Holds the show more button element
+var countryCards = document.getElementById("country-cards-container");
+
 searchInput.addEventListener("input", filterData); // Listens for the input in the name search bar and calls filterData
 regionInput.addEventListener("change", filterData); // Listens for the changing of the selected region and calls filterData
 populationInput.addEventListener("input", filterData); // Listens for the input in the population search bar and calls filterData
 showMorePressed.addEventListener("click", showMoreHandler); // Listens for the clicking of the show more button and calls showMoreHandler
+
+// listens for the selction of any country cards
+document.addEventListener('click', function(event) {
+    console.clear();
+    if(event.target.matches(".country-card")){
+        console.log("I detected: " + event);
+        countryCardHandler(filteredCountries[event.target.id]);
+    }
+});
+
+/**
+ * Function: countryCardHandler
+ * @param {*} country 
+ * Description: Takes an object of country an creates a query string and sends the query to the details page to be displayed
+ */
+function countryCardHandler(country) {
+    let queryString = "?name=" + encodeURIComponent(country.name.official) + "&flag=" + encodeURIComponent(country.flags.svg) + "&population=" + country.population + "&region=" + country.region + "&subRegion=" + encodeURIComponent(country.subregion) + "&capital=" + encodeURIComponent(country.capital) + "&currencies=" + encodeURIComponent(getFormattedNames(country.currencies)) + "&languages=" + encodeURIComponent(getFormattedNames(country.languages));
+    window.location.href = ("detail.html" + queryString);
+}
 
 /**
  * Function: showMoreHandler
@@ -34,27 +55,43 @@ function showMoreHandler(){
 function filterData(){
     let regex = new RegExp (/^[a-zA-Z -]*$/);
     if (!regex.test (searchInput.value) && !stringIsBlank(searchInput.value)){
-        console.log("Enter valid Country Name");
+        console.log("Invalid country name entered");
         showErrorMessage("name-error");
         return false;
     } else if (!stringIsBlank(searchInput.value) && populationIsValid(populationInput.valueAsNumber)){
+        console.clear();
         console.log("1- I found the input: " + searchInput.value);
         filteredCountries = applyFiltersSearch(searchInput.value.toLowerCase(), isAllRegions(regionInput.value), regionInput.value, populationInput.valueAsNumber);
         hideErrorMessage("name-error");
         hideErrorMessage("pop-error");
         countryCount = 12;
     } else if (stringIsBlank(searchInput.value) && populationIsValid(populationInput.valueAsNumber)){
+        console.clear();
         console.log("2- I found the input: " + populationInput.valueAsNumber);
         filteredCountries = applyFiltersNoSearch(isAllRegions(regionInput.value), regionInput.value, populationInput.valueAsNumber);
         hideErrorMessage("name-error");
         hideErrorMessage("pop-error");
         countryCount = 12;
     } else {
-        console.log("Invalid population number");
+        console.log("Invalid population number entered");
         showErrorMessage("pop-error");
         return false;
     }
     populateCountryCards(filteredCountries, countryCount);
+}
+
+/**
+ * Function: getFormattedNames
+ * @param {*} item 
+ * Decription: receives a list passed from an object of country and creates a formatted sub-string to be returned
+ * @returns output.substring(0, output.length-2)
+ */
+function getFormattedNames(item){
+    let output = "";
+    for(let index = 0; index < item.length; index++){
+        output += (item[index].name + ", ");
+    }
+    return output.substring(0, output.length-2);
 }
 
 /**
@@ -208,6 +245,7 @@ function populateCountryCards(countriesArray, total) {
             let newCard = document.createElement("div");
             newCard.name = 'card';
             newCard.className = 'country-card';
+            newCard.id = loop;
 
             // Creates each image element for each new flag
             let flag = document.createElement("img");
